4f293546f20b2029f9299e521289a08478a8a0f1
Virginia <virginia_ortiz_rodriguez@hotmail.com>
2022-01-20 09:30:30 +0100

    Pide un texto e indica si empieza por H

---
+++ Cadenas/src/ejer1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ejer1 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("introduce un texto");
+		String cadena = entrada.nextLine();
+
+		if (cadena.charAt(0)=='H') {
+			System.out.println(" la cadena empieza en H");
+		} else  {
+			System.out.println("La cadena no comienza con H");
+		}
+	}
+
+	}
+
